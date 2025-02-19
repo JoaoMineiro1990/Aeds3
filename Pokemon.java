0b7c16f18188505dce4e78ced16d4fd28fb9c876
João Vitor Mendes Moreira <1454474@sga.pucminas.br>
2025-02-19 13:28:05 -0300

    adicionando algumas coisas nesse projeto que vai ser muito facil

---
+++ Pokemon.java
@@ -0,0 +1,166 @@
+public class Pokemon {
+        // Atributos da classe
+        private String abilities;
+        private int attack;
+        private int baseEggSteps;
+        private int baseTotal;
+        private int captureRate;
+        private int defense;
+        private double heightM;
+        private int hp;
+        private String name;
+        private int pokedexNumber;
+        private int spAttack;
+        private int spDefense;
+        private int speed;
+        private String type1;
+        private String type2;
+        private double weightKg;
+        private int generation;
+        private boolean isLegendary;
+        
+// Getters and Setters
+public String getAbilities() {
+        return abilities;
+}
+
+public void setAbilities(String abilities) {
+        this.abilities = abilities;
+}
+
+public int getAttack() {
+        return attack;
+}
+
+public void setAttack(int attack) {
+        this.attack = attack;
+}
+
+public int getBaseEggSteps() {
+        return baseEggSteps;
+}
+
+public void setBaseEggSteps(int baseEggSteps) {
+        this.baseEggSteps = baseEggSteps;
+}
+
+public int getBaseTotal() {
+        return baseTotal;
+}
+
+public void setBaseTotal(int baseTotal) {
+        this.baseTotal = baseTotal;
+}
+
+public int getCaptureRate() {
+        return captureRate;
+}
+
+public void setCaptureRate(int captureRate) {
+        this.captureRate = captureRate;
+}
+
+public int getDefense() {
+        return defense;
+}
+
+public void setDefense(int defense) {
+        this.defense = defense;
+}
+
+public double getHeightM() {
+        return heightM;
+}
+
+public void setHeightM(double heightM) {
+        this.heightM = heightM;
+}
+
+public int getHp() {
+        return hp;
+}
+
+public void setHp(int hp) {
+        this.hp = hp;
+}
+
+public String getName() {
+        return name;
+}
+
+public void setName(String name) {
+        this.name = name;
+}
+
+public int getPokedexNumber() {
+        return pokedexNumber;
+}
+
+public void setPokedexNumber(int pokedexNumber) {
+        this.pokedexNumber = pokedexNumber;
+}
+
+public int getSpAttack() {
+        return spAttack;
+}
+
+public void setSpAttack(int spAttack) {
+        this.spAttack = spAttack;
+}
+
+public int getSpDefense() {
+        return spDefense;
+}
+
+public void setSpDefense(int spDefense) {
+        this.spDefense = spDefense;
+}
+
+public int getSpeed() {
+        return speed;
+}
+
+public void setSpeed(int speed) {
+        this.speed = speed;
+}
+
+public String getType1() {
+        return type1;
+}
+
+public void setType1(String type1) {
+        this.type1 = type1;
+}
+
+public String getType2() {
+        return type2;
+}
+
+public void setType2(String type2) {
+        this.type2 = type2;
+}
+
+public double getWeightKg() {
+        return weightKg;
+}
+
+public void setWeightKg(double weightKg) {
+        this.weightKg = weightKg;
+}
+
+public int getGeneration() {
+        return generation;
+}
+
+public void setGeneration(int generation) {
+        this.generation = generation;
+}
+
+public boolean isLegendary() {
+        return isLegendary;
+}
+
+public void setLegendary(boolean isLegendary) {
+        this.isLegendary = isLegendary;
+}
+}
